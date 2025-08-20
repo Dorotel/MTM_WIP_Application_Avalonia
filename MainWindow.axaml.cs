d351f22cae895128c170fa52d62b24b96f94e05f
Dorotel <149977176+Dorotel@users.noreply.github.com>
2025-08-19 23:06:13 -0500

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MTM_WIP_Application_Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
